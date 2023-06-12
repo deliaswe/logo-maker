038798f8d899e2964a8ecce949099f22ffa50e43
delia <you@example.com>
2023-06-11 21:38:40 -0400

    requiring npm package

---
+++ index.js
@@ -0,0 +1,10 @@
+// requiring npm packages
+const inquirer = require('inquire');
+const fs = require('fs');
+const square = require('./lib/square');
+const triangle = ('./lib/triangle.js');
+const circle = require('./lib/circle');
+
+
+
+
